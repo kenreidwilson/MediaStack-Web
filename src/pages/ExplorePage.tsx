b6075593a6443c3d65333808c376324c7f032f6e
kenreidwilson <kenreidwilson@gmail.com>
2021-10-19 10:34:48 -0400

    Started mocking repos.

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,32 +1,18 @@
-import Media from '../types/Media';
-import IMediaUpdateRequest from '../types/IMediaUpdateRequest';
-import React, { useState, useEffect } from 'react';
-import useMedia from '../hooks/useMedia';
-import Navigation from '../components/Misc/Navigation';
-import MediaUpdateForm from '../components/Forms/MediaUpdateForm';
+import { useEffect } from 'react';
+import BasePage from './BasePage';
+import MockTagRepository from '../repositories/Mocks/MockTagRepository';
 
 export default function ExplorePage() {
 
-    const [mediaList, setMediaList] = useState<Media[]>([]);
-    const [selectedMedia, setSelectedMedia] = useState<Media[]>([]);
-    const [updateRequest, setUpdateRequest] = useState<IMediaUpdateRequest>( { ID: -1 } );
-
-    const { search, update } = useMedia();
+    const mtr = new MockTagRepository();
 
     useEffect(() => {
-        search({}).then(response => {
-            setMediaList(response.data);
-        });
+        mtr.get(3).then(console.log);
     }, [])
 
-    const updateMedia = (score: number) => {
-        return Promise.all(selectedMedia.map(m => update({ ID: m.id, score, tagIDs: [1] })));
-    }
-
     return (
-        <React.Fragment>
-            <Navigation />
-            {mediaList.length > 0 ? <MediaUpdateForm request={updateRequest} onChange={setUpdateRequest} /> : null}
-        </React.Fragment>
+        <BasePage>
+            <h2></h2>
+        </BasePage>
     );
 };
